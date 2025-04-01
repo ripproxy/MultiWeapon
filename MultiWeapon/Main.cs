4da7ddd20dc4794aead80f57ba41c3ee23143894
ripproxy <ripproxy11@gmail.com>
2025-04-02 05:15:22 +0800

    Update Main.cs

--- MultiWeapon/Main.cs
+++ MultiWeapon/Main.cs
@@ -1,67 +1,121 @@
-using System;
-using System.Collections.Generic;
 using Terraria;
 using TerrariaApi.Server;
 using TShockAPI;
-using TShockAPI.Hooks;
+using Microsoft.Xna.Framework;
 
-namespace MultiWeapon
+[ApiVersion(2, 1)]
+public class SyncedAttack : TerrariaPlugin
 {
-    [ApiVersion(2, 1)]
-    public class MultiWeapon : TerrariaPlugin
+    private Dictionary<int, Vector2> attackDirections = new Dictionary<int, Vector2>();
+    
+    public override string Author => "Nama Anda";
+    public override string Description => "3 senjata dengan arah serangan sama";
+    public override string Name => "SyncedAttack";
+    public override Version Version => new Version(1, 3, 0);
+
+    public SyncedAttack(Main game) : base(game) { }
+
+    public override void Initialize()
     {
-        public override string Name => "MultiWeapon";
-        public override Version Version => new Version(1, 0);
-        public override string Author => "YourName";
-        public override string Description => "Allows simultaneous weapon attacks from slots 0-2";
+        GetDataHandlers.PlayerUpdate.Register(this, OnPlayerUpdate);
+        GetDataHandlers.PlayerItemAnimation.Register(this, OnItemAnimation);
+    }
 
-        public MultiWeapon(Main game) : base(game)
+    private void OnPlayerUpdate(object sender, GetDataHandlers.PlayerUpdateEventArgs args)
+    {
+        // Simpan posisi mouse saat attack
+        if (args.Control.IsUsingItem)
         {
+            attackDirections[args.PlayerId] = new Vector2(args.MouseX, args.MouseY);
         }
+    }
 
-        public override void Initialize()
+    private void OnItemAnimation(object sender, GetDataHandlers.PlayerItemAnimationEventArgs args)
+    {
+        if (args.ItemAnimationType != 1 || !attackDirections.ContainsKey(args.Player.Index))
+            return;
+
+        var player = args.Player;
+        var mainSlot = player.TPlayer.selectedItem;
+        var mousePos = attackDirections[args.Player.Index];
+
+        if (mainSlot < 0 || mainSlot > 2)
+            return;
+
+        for (int slot = 0; slot < 3; slot++)
         {
-            PlayerHooks.PlayerPostUpdate += OnPlayerPostUpdate;
-        }
-
-        private void OnPlayerPostUpdate(PlayerPostUpdateEventArgs e)
-        {
-            if (e.Player == null || !e.Player.Active || e.Player.SelectedItem < 0 || e.Player.SelectedItem > 2)
-                return;
-
-            var player = e.Player.TPlayer;
-            var tsPlayer = TShock.Players[e.Player.Index];
-
-            if (player.controlUseItem && player.itemAnimation == 0 && player.releaseUseItem)
+            if (slot == mainSlot) continue;
+            
+            Item weapon = player.TPlayer.inventory[slot];
+            if (IsValidWeapon(weapon))
             {
-                var originalSlot = player.selectedItem;
-                var originalDirection = player.direction;
-
-                for (int i = 0; i < 3; i++)
-                {
-                    var item = player.inventory[i];
-                    if (item.damage > 0 && item.pick == 0 && item.axe == 0 && item.hammer == 0)
-                    {
-                        player.selectedItem = i;
-                        player.direction = originalDirection;
-                        player.itemTime = 0;
-                        player.ItemCheck();
-                    }
-                }
-
-                player.selectedItem = originalSlot;
-                tsPlayer.SendData(PacketTypes.PlayerSlot, "", player.whoAmI, originalSlot, 0);
-                tsPlayer.SendData(PacketTypes.PlayerControls, "", player.whoAmI);
+                // Simulasi dengan arah serangan sama
+                SyncAttackDirection(player, slot, weapon, mousePos);
             }
         }
+    }
 
-        protected override void Dispose(bool disposing)
+    private void SyncAttackDirection(TSPlayer player, int slot, Item weapon, Vector2 mousePos)
+    {
+        // Konversi koordinat mouse relatif ke world position
+        Vector2 worldMouse = new Vector2(
+            player.TPlayer.position.X + mousePos.X - Main.screenWidth/2,
+            player.TPlayer.position.Y + mousePos.Y - Main.screenHeight/2
+        );
+
+        // Simpan state asli
+        Vector2 originalMouse = new Vector2(player.TPlayer.position.X, player.TPlayer.position.Y);
+        int originalSlot = player.TPlayer.selectedItem;
+
+        try
         {
-            if (disposing)
+            // Override posisi mouse dan slot
+            player.TPlayer.selectedItem = slot;
+            player.TPlayer.controlUseItem = true;
+            
+            // Proses arah serangan
+            player.TPlayer.direction = (worldMouse.X > player.TPlayer.Center.X) ? 1 : -1;
+            weapon.UseItem(player.Index);
+            
+            // Proyektil khusus
+            if (weapon.shoot > 0)
             {
-                PlayerHooks.PlayerPostUpdate -= OnPlayerPostUpdate;
+                Vector2 velocity = Vector2.Normalize(worldMouse - player.TPlayer.Center) * weapon.shootSpeed;
+                Projectile.NewProjectile(
+                    player.TPlayer.GetProjectileSource_Item(weapon),
+                    player.TPlayer.Center,
+                    velocity,
+                    weapon.shoot,
+                    weapon.damage,
+                    weapon.knockBack,
+                    player.Index
+                );
             }
-            base.Dispose(disposing);
+
+            // Update animasi
+            NetMessage.SendData((int)PacketTypes.PlayerItemAnimation, -1, -1, 
+                NetworkText.Empty, player.Index, slot, 1);
         }
+        finally
+        {
+            // Reset state
+            player.TPlayer.selectedItem = originalSlot;
+        }
+    }
+
+    private bool IsValidWeapon(Item item)
+    {
+        return item.active && item.damage > 0 && item.pick == 0 && 
+               item.axe == 0 && item.hammer == 0 && !item.notAmmo;
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            GetDataHandlers.PlayerUpdate.UnRegister(this, OnPlayerUpdate);
+            GetDataHandlers.PlayerItemAnimation.UnRegister(this, OnItemAnimation);
+        }
+        base.Dispose(disposing);
     }
 }
