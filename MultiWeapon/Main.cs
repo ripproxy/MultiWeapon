435832ac7ce4522e8ae0a86d7390e60ed4134e7b
ripproxy <ripproxy11@gmail.com>
2025-04-02 04:03:08 +0800

    Update Main.cs

--- MultiWeapon/Main.cs
+++ MultiWeapon/Main.cs
@@ -1,109 +1,67 @@
 using System;
-using Microsoft.Xna.Framework;
-using TShockAPI;
+using System.Collections.Generic;
 using Terraria;
 using TerrariaApi.Server;
-using Terraria.Localization;
+using TShockAPI;
+using TShockAPI.Hooks;
 
-namespace MultiWeaponPlugin
+namespace MultiWeapon
 {
     [ApiVersion(2, 1)]
-    public class MultiWeaponPlugin : TerrariaPlugin
+    public class MultiWeapon : TerrariaPlugin
     {
-        public override string Name => "MultiWeaponPlugin";
+        public override string Name => "MultiWeapon";
+        public override Version Version => new Version(1, 0);
         public override string Author => "YourName";
-        public override string Description => "Plugin untuk menyerang dengan 3 senjata sekaligus";
-        public override Version Version => new Version(1, 0, 1);
+        public override string Description => "Allows simultaneous weapon attacks from slots 0-2";
 
-        private readonly int[] weaponSlots = { 0, 1, 2 };
-        private const int DefaultMeleeProj = 15; // ID proyektil untuk slash pedang dasar
-
-        public MultiWeaponPlugin(Main game) : base(game) { }
+        public MultiWeapon(Main game) : base(game)
+        {
+        }
 
         public override void Initialize()
         {
-            ServerApi.Hooks.NetGetData.Register(this, OnGetData);
+            PlayerHooks.PlayerPostUpdate += OnPlayerPostUpdate;
+        }
+
+        private void OnPlayerPostUpdate(PlayerPostUpdateEventArgs e)
+        {
+            if (e.Player == null || !e.Player.Active || e.Player.SelectedItem < 0 || e.Player.SelectedItem > 2)
+                return;
+
+            var player = e.Player.TPlayer;
+            var tsPlayer = TShock.Players[e.Player.Index];
+
+            if (player.controlUseItem && player.itemAnimation == 0 && player.releaseUseItem)
+            {
+                var originalSlot = player.selectedItem;
+                var originalDirection = player.direction;
+
+                for (int i = 0; i < 3; i++)
+                {
+                    var item = player.inventory[i];
+                    if (item.damage > 0 && item.pick == 0 && item.axe == 0 && item.hammer == 0)
+                    {
+                        player.selectedItem = i;
+                        player.direction = originalDirection;
+                        player.itemTime = 0;
+                        player.ItemCheck();
+                    }
+                }
+
+                player.selectedItem = originalSlot;
+                tsPlayer.SendData(PacketTypes.PlayerSlot, "", player.whoAmI, originalSlot, 0);
+                tsPlayer.SendData(PacketTypes.PlayerControls, "", player.whoAmI);
+            }
         }
 
         protected override void Dispose(bool disposing)
         {
             if (disposing)
-                ServerApi.Hooks.NetGetData.Deregister(this, OnGetData);
-            base.Dispose(disposing);
-        }
-
-        private void OnGetData(GetDataEventArgs args)
-        {
-            if (args.Handled || (int)args.MsgID != 41)
-                return;
-
-            int playerIndex = args.Msg.whoAmI;
-            TSPlayer tsPlayer = TShock.Players[playerIndex];
-            if (tsPlayer?.Active != true)
-                return;
-
-            int selectedSlot = tsPlayer.TPlayer.selectedItem;
-            if (Array.IndexOf(weaponSlots, selectedSlot) == -1)
-                return;
-
-            Item triggerItem = tsPlayer.TPlayer.inventory[selectedSlot];
-            if (triggerItem?.damage <= 0)
-                return;
-
-            foreach (int slot in weaponSlots)
             {
-                if (slot == selectedSlot)
-                    continue;
-
-                Item weaponItem = tsPlayer.TPlayer.inventory[slot];
-                if (weaponItem?.damage <= 0)
-                    continue;
-
-                // ========== PERUBAHAN UTAMA ==========
-                int projType = weaponItem.shoot;
-                bool isMelee = weaponItem.melee;
-
-                // Handle senjata melee tanpa proyektil
-                if (isMelee && projType == 0)
-                    projType = DefaultMeleeProj;
-
-                // Skip jika tetap tidak ada proyektil
-                if (projType == 0)
-                    continue;
-                // =====================================
-
-                Vector2 pos = tsPlayer.TPlayer.Center;
-                float speed = 10f;
-                float rotation = tsPlayer.TPlayer.itemRotation;
-                if (tsPlayer.TPlayer.direction == -1)
-                    rotation += MathHelper.Pi;
-
-                Vector2 velocity = new Vector2((float)Math.Cos(rotation), (float)Math.Sin(rotation)) * speed;
-                int damage = Math.Max(weaponItem.damage, 10);
-                float knockBack = Math.Max(weaponItem.knockBack, 2f);
-
-                // Generate proyektil
-                int projId = Projectile.NewProjectile(
-                    null,
-                    pos.X,
-                    pos.Y,
-                    velocity.X,
-                    velocity.Y,
-                    projType,
-                    damage,
-                    knockBack,
-                    tsPlayer.Index
-                );
-
-                NetMessage.SendData(
-                    (int)PacketTypes.ProjectileNew,
-                    -1,
-                    -1,
-                    NetworkText.FromLiteral(""),
-                    projId,
-                    0f, 0f, 0f, 0
-                );
+                PlayerHooks.PlayerPostUpdate -= OnPlayerPostUpdate;
             }
+            base.Dispose(disposing);
         }
     }
 }
